676294d66529286c21051b9e4b24095426f0c284
pritom-dey1 <Pritom1.2.zx@gmail.com>
2025-12-12 03:51:05 +0600

    added domains

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(cookieParser())
 app.use(morgan("dev"))
 app.use(
   cors({
-    origin: "https://superb-panda-478822.netlify.app",
+    origin: "https://cute-dusk-f1718c.netlify.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
